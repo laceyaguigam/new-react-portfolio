604d7b30e31cb45ba70c2fff040798335691361a
lacey aguigam <laceyaguigam@yahoo.com>
2022-11-20 22:43:55 -0800

    added import and footer function to footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+import react from "react"
+
+function Footer () {
+    return (
+        <article className="contact-me" id="contact-me">
+      <h3>Contact Me</h3>
+      <div className="contact-info">
+        <ul>
+          <li>
+            559.589.4952
+          </li>
+          <li>
+            laceyaguigam@yahoo.com
+          </li>
+          <li>
+            <a href="https://github.com/laceyaguigam">GitHub</a>
+          </li>
+        </ul>
+      </div>
+    </article>
+    )
+} 
+export default Footer
