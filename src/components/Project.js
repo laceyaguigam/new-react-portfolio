027ae05980bf242cac15de7b8f9ef16eda66a197
lacey aguigam <laceyaguigam@yahoo.com>
2022-11-20 22:46:44 -0800

    imported photos

---
+++ src/components/Project.js
@@ -0,0 +1,12 @@
+import react from "react"
+import img2 from '../assets/images/Heriseon-Marketing.png'
+import img3 from '../assets/images/Holiday-Calendar.png'
+import img5 from '../assets/images/placeholder-3.png'
+import img6 from '../assets/images/Run-Buddy.png'
+import img7 from '../assets/images/Smart-Homie.png'
+
+
+
+
+
+
